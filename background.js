2d582bfad94d6e1b2aafed88edec60989ae81463
changdf <2210992483@qq.com>
2016-03-02 22:25:06 +0800

    Revert ":octocat: Added .gitattributes"
    This reverts commit a8ef684084bf28082234c2e0d28b33d8c06467f7.

---
+++ background.js
@@ -0,0 +1,159 @@
+﻿//background.js仅在加载插件时执行一次
+var settings = {
+	crawlName:'cdf',
+	interval:0.05,
+	//Webpage Screenshot扩展ID
+	externalId:"haofdcifapgaogjhhnmaelbcbbemmakc",
+	//截图扩展的tab地址
+	externalUrl:"editor.html"
+}
+var crawlLevel = 1;
+//建立websocket连接
+//var ws = new WebSocket("ws://localhost");//http使用
+//var ws = new WebSocket("wss://localhost");//https使用
+var socket = io.connect('http://localhost');
+//定时请求CrawlUrl
+function getCrawlUrl(){
+	//获取TabCount
+	chrome.tabs.query({}, function(tabs) {
+		var tabCount = tabs.length;
+		var tmpSocket = socket.emit('getCrawlUrl', {crawlName: settings.crawlName,tabCount:tabCount});
+		tmpSocket.sendBuffer = [];
+	});
+}
+chrome.alarms.create('getCrawlUrl', { periodInMinutes: settings.interval});
+//setInterval("getCrawlUrl()",settings.interval);
+socket.on('getCrawlUrl', function (data) {
+	if(data.crawingUrl!=null){
+		chrome.tabs.create({url:data.crawingUrl})
+	}
+});
+//如果socket可以连通，则将storage的内容发送到后端保存
+if(socket.connected){
+	chrome.storage.local.get("post_url",function(data){
+		if(data.post_url!=null){
+			socket.emit("post_url",{crawlName:settings.crawlName,post_urlList:data.post_url});
+		}
+	})
+	chrome.storage.local.get("post_detail_data",function(data){
+		if(data.post_detail_data!=null){
+			socket.emit("post_detail_data",{crawlName:settings.crawlName,post_detail_data:data.post_detail_data});
+		}
+	})
+}
+
+//接收content_script发送的消息提交到服务器
+chrome.extension.onMessage.addListener(function(request, sender, sendRequest){
+	request.crawlName = settings.crawlName;
+	//判断消息类型
+	if(request.messageType=='post_url'){
+		var tmpSocket = socket.emit("post_url",{crawlName:settings.crawlName,post_urlList:request.post_urlList});
+		tmpSocket.send();
+		if(!tmpSocket.connected){
+			//清空sendBuffer
+			tmpSocket.sendBuffer = [];
+		}
+		//如果网络中断则保存在storage
+		if(!tmpSocket.connected){
+			chrome.storage.local.get("post_url",function(data){
+				if(data.post_url==null){
+					chrome.storage.local.set({post_url:request.post_urlList}, function(){
+					});
+				}else{
+					var tmpArray = data.post_url.concat(request.post_urlList);
+					chrome.storage.local.set({post_url:tmpArray}, function(){
+					});
+				}
+			})
+		}
+		//截图
+		chrome.extension.sendRequest(settings.externalId,{},function(response){
+			//关闭Tab释放资源
+			if(response!=null){
+				chrome.tabs.remove(sender.tab.id);
+			}
+		});
+	}else if(request.messageType=='post_detail_data'){
+		var tmpSocket = socket.emit('post_detail_data',{crawlName:settings.crawlName,'post_detail_data':request.post_detail_data});
+		tmpSocket.send();
+		if(!tmpSocket.connected){
+			//清空sendBuffer
+			tmpSocket.sendBuffer = [];
+		}
+		//如果网络中断则保存在storage
+		if(!tmpSocket.connected){
+			chrome.storage.local.get("post_detail_data",function(data){
+				if(data.post_detail_data==null){
+					chrome.storage.local.set({post_detail_data:request.post_detail_data}, function(){
+					});
+				}else{
+					var tmpArray = data.post_detail_data.concat(request.post_detail_data);
+					chrome.storage.local.set({post_detail_data:tmpArray}, function(){
+						
+					});
+				}
+			})
+		}
+	}else if(request.messageType=='errorUrl'){
+		socket.emit('errorUrl', request);
+	}else if(request.messageType=='closeTab'){
+		//截图
+		chrome.extension.sendRequest(settings.externalId,{url:sender.tab.url},function(response){
+			//关闭Tab释放资源
+			chrome.tabs.remove(sender.tab.id);
+		});
+	}
+});
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+    if (alarm && alarm.name == 'getCrawlUrl') {
+        getCrawlUrl();
+    }else if(alarm && alarm.name == 'downloadImage'){
+	    downloadImage();
+    }else if(alarm && alarm.name == 'clickCommentPage'){
+	    
+    }
+});
+
+//定时将截取的图片下载到本地保存
+chrome.alarms.create('downloadImage', {periodInMinutes: settings.interval});
+function downloadImage(){
+	chrome.tabs.query({}, function(tabs) {
+		for(var i=0;i<tabs.length;i++){
+			if(tabs[i].url.indexOf(settings.externalUrl)!=-1){
+				//保存图片
+				chrome.tabs.executeScript(tabs[i],{code:"getImageSrc();", allFrames: true},function(imageSrc){
+					if(imageSrc.length!=0){
+						chrome.downloads.download({url: imageSrc[0]});
+						chrome.tabs.remove(tabs[i].id);
+					}
+				});
+			}
+		}
+	});
+}
+
+//定时点击某个评论页面
+/*chrome.alarms.create('clickCommentPage', { periodInMinutes: settings.interval});
+function clickCommentPage(){
+	chrome.tabs.query({}, function(tabs) {
+		for(var i=0;i<tabs.length;i++){
+			if((tabs[i].url.indexOf("detail.tmall.com/item.htm")!=-1)
+			|| (tabs[i].url.indexOf("taobao.com/item")!=-1)){
+				//获取下一页的评论
+				chrome.tabs.executeScript(tabs[i],{code:"nextPage();", allFrames: true},function(postData){
+					if(postData=="exception"){
+						//将异常URL地址保存到服务器
+						socket.emit('errorUrl', {crawlName:settings.crawlName,errorUrl:tabs[i].url});
+					}else if(postData!=null && postData.length!=0){
+						//保存数据到服务器（注：此处和之前的代码基本重复，为了测试方便，暂且不提取出来）
+						socket.emit('post_detail_data',{crawlName:settings.crawlName,'post_detail_data':postData});
+					}else if(postData==null){
+						chrome.tabs.remove(tabs[i].id);
+					}
+				});
+			}
+		}
+	});
+}*/
+
---
+++ content_script.js
@@ -0,0 +1,210 @@
+﻿//页面加载完毕再执行
+$(function(){
+	var settings = {
+	maxCommentPage:50,
+	sleepTime:2000,
+	//如果获取不到元素，尝试的次数
+	tryCount:2,
+	//用于判断是否是所要抓取的域名
+	crawlDomain:["taobao","tmall"],
+	loginDomain:"login.taobao.com"
+}
+	function tryAgain(f){
+		for(var i=0;i<settings.tryCount;i++){
+			//setTimeout(f,settings.sleepTime);
+			sleepFor(settings.sleepTime);
+			if((f()!=null && !(f() instanceof Array)) || (f() instanceof Array && f().length!=0)){
+				break;
+			}
+		}
+	}
+
+	//获取当前要抓取的网址
+	var crawlUrl = window.location.href;
+	var isCrawlDomain = false;
+	for(var i=0;i<settings.crawlDomain.length;i++){
+		if(crawlUrl.indexOf(settings.crawlDomain[i])!=-1){
+			isCrawlDomain = true;
+			break;
+		}
+	}
+
+	if(!isCrawlDomain){
+		return;
+	}
+	//判断是否需要重新登录
+	if(crawlUrl.indexOf(settings.loginDomain)!=-1){
+		document.getElementById("TPL_username_1").value = "13716183704";
+		document.getElementById("TPL_password_1").value = "cdf12020cdf12020";
+		document.getElementById("J_SubmitStatic").click();
+	}
+
+	/*document.body.scrollTop = document.body.scrollHeight/2;
+	document.body.scrollTop = document.body.scrollHeight/2;*/
+	//保证页面所有ajax内容加载完毕
+	while(document.querySelector(".footer")==null){
+		//do nothing
+	}
+	var footerDom = document.querySelector(".footer");
+	footerDom.scrollIntoView();
+	footerDom.scrollIntoView();
+	footerDom.scrollIntoView();
+
+	//检查当前链接是否正常抓取，如果没有，则保存到服务器
+	var isException = false;
+
+	//根据url地址解析内容或根据模式匹配解析内容
+	if((crawlUrl.indexOf("s.taobao.com/search")!=-1 && crawlUrl.indexOf("search_type")!=-1)
+		|| (crawlUrl.indexOf("s.taobao.com/list")!=-1 && crawlUrl.indexOf("app=vproduct")!=-1)
+	){
+		//搜索页
+		var allUrl = document.querySelectorAll("div.title-row > a");
+		//获取不到可能是网页没有加载出来，sleep一下
+		if(allUrl.length==0){
+			var f = function(){
+				return document.querySelectorAll("div.title-row > a");
+			}
+			tryAgain(f);
+		}
+		allUrl = document.querySelectorAll("div.title-row > a");
+		var urlList = [];
+		for(var i=0;i<allUrl.length;i++){
+			urlList.push(allUrl[i].href);
+		}
+		//scrollIntoView以便于截图
+		if(allUrl.length!=0){
+			allUrl[0].scrollIntoView();
+		}
+		if(urlList.length!=0){
+			chrome.extension.sendMessage({messageType:"post_url","post_urlList":urlList});
+		}
+		else{
+			isException = true;
+			chrome.extension.sendMessage({messageType:"errorUrl","errorUrl":crawlUrl});
+		}
+	}else if((crawlUrl.indexOf("s.taobao.com/search")!=-1 && crawlUrl.indexOf("spu_title")!=-1)
+	||(crawlUrl.indexOf("s.taobao.com/list")!=-1 && crawlUrl.indexOf("app=detailproduct")!=-1)
+	){
+		//搜索结果页
+		var allUrl = document.querySelectorAll("div.col.col-2 > p > a");
+		//获取不到可能是网页没有加载出来，sleep一下
+		if(allUrl.length==0){
+			var f = function(){
+				return document.querySelectorAll("div.col.col-2 > p > a");
+			}
+			tryAgain(f);
+		}
+		allUrl = document.querySelectorAll("div.col.col-2 > p > a");
+		var urlList = [];
+		for(var i=0;i<allUrl.length;i++){
+			urlList.push(allUrl[i].href);
+		}
+		//scrollIntoView以便于截图
+		if(allUrl.length!=0){
+			allUrl[0].scrollIntoView();
+		}
+		if(urlList.length!=0){
+			chrome.extension.sendMessage({messageType:"post_url","post_urlList":urlList});
+		}
+		else{
+			isException = true;
+			chrome.extension.sendMessage({messageType:"errorUrl","errorUrl":crawlUrl});
+		}
+	}else if((crawlUrl.indexOf("detail.tmall.com/item.htm")!=-1)
+			|| (crawlUrl.indexOf("taobao.com/item")!=-1)
+	){
+		//详情页（包含评论）
+		//获取不到可能是网页没有加载出来，sleep一下
+		if(document.querySelector("#J_Title > h3")==null){
+			var f = function(){
+				return document.querySelector("#J_Title > h3");
+			}
+			tryAgain(f);
+		}
+		//获取产品名称
+		var productName = null;
+		if(document.querySelector("#J_Title > h3")!=null){
+			productName = document.querySelector("#J_Title > h3").innerText;
+		}
+		//获取评论(最多抓取50页)
+		var comment_click = document.evaluate("//a[contains(text(),'累计评论')]",document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null).snapshotItem(0);
+		if(comment_click!=null){
+			comment_click.click();
+		}
+		//ajax分页
+		var pageCount = 0;
+		var commentCount = 1;
+		var sumCommentCount = parseInt(comment_click.querySelector("em").innerText);
+		if(sumCommentCount!=0){
+			var f = function(){
+				//获取第一页的所有评论数
+				var tmpCommentPath = document.evaluate("//span[contains(text(),'有用')]/../../../../div[1]/text()",document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
+				var length = tmpCommentPath.snapshotLength;
+				var postData = [];
+				for(var i=0;i<length;i++){
+					postData.push(tmpCommentPath.snapshotItem(i).textContent);
+				}
+				if(postData.length==0){
+					isException = true;
+					chrome.extension.sendMessage({messageType:"errorUrl","errorUrl":crawlUrl});
+				}else{
+					//提交评论数据
+					chrome.extension.sendMessage({messageType:"post_detail_data","post_detail_data":postData});
+				}
+			}
+
+			//定时检测页面内容是否加载完毕，如果加载完毕则发送自定义事件pageLoad
+			var f1 = function () {
+				if(document.evaluate("//span[contains(text(),'有用')]",document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null).snapshotItem(0)!=null){
+					clearInterval(m);
+					var event = document.createEvent('HTMLEvents');
+					event.initEvent('pageLoad', true, false);
+					event.eventType = 'comment';
+					document.dispatchEvent(event);
+				}
+			};
+			var m = setInterval(f1, 1000);
+
+			document.addEventListener('pageLoad', function (event) {
+				if(event.eventType=="comment"){
+					f();
+					//下一页
+					var nextPage = document.evaluate("//li[contains(text(),'下一页')]",document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null).snapshotItem(0);
+					if(nextPage==null || nextPage.className.indexOf("disabled")!=-1){
+						//所有页数已经抓取完毕
+						chrome.extension.sendMessage({messageType:"closeTab"});
+						return;
+					}else{
+						nextPage.click();
+						m = setInterval(f1, 1000);
+					}
+				}
+			}, false);
+		}
+	}
+
+	/*if(isException){
+		chrome.extension.sendMessage({messageType:"errorUrl","errorUrl":crawlUrl});
+	}*/
+	function sleepFor(sleepDuration){
+	    var now = new Date().getTime();
+	    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+	}
+
+});
+
+function getImageSrc(){
+	return document.getElementById("imgFixForLong").src;
+}
+
+
+
+
+
+
+
+
+
+
+
+
